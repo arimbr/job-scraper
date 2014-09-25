beb079ea91f7b0678bf1911d9b74fb3ed67baf1b
ari <arimbr@gmail.com>
2014-09-25 14:03:04 +0300

    Define Job Item

--- stackoverflow/stackoverflow/items.py
+++ stackoverflow/stackoverflow/items.py
@@ -8,7 +8,5 @@
 import scrapy
 
 
-class StackoverflowItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JobItem(scrapy.Item):
+    id = scrapy.Field()
