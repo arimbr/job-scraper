bcfa8755959a55029b8cf99967417c94b31a0c85
ari <arimbr@gmail.com>
2014-09-25 13:51:55 +0300

    Create Scrapy project

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ job-scraper
 ===========
 
 Scraping jobs data from Stack Overflow Careers
+
+Requried packages:
+
+sudo apt-get install libxml2-dev libxslt-dev python-dev
+
+Instructions:
+
+virtualenv env
+source env/bin/activate
+pip install lxml
+sudo apt-get install libffi-dev
+pip install Scrapy
---
+++ stackoverflow/stackoverflow/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for stackoverflow project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'stackoverflow'
+
+SPIDER_MODULES = ['stackoverflow.spiders']
+NEWSPIDER_MODULE = 'stackoverflow.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'stackoverflow (+http://www.yourdomain.com)'
