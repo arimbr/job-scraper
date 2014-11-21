d74c4dd941563467f9c81d54270ca4c3fcdb68b4
arimbr <arimbr@gmail.com>
2014-11-21 18:59:08 +0200

    Add request to detail page

--- stackoverflow/stackoverflow/spiders/job_spider.py
+++ stackoverflow/stackoverflow/spiders/job_spider.py
@@ -1,7 +1,5 @@
 import scrapy
 
-from scrapy.exceptions import CloseSpider
-
 from stackoverflow.items import JobItem
 
 
@@ -10,24 +8,30 @@ class JobSpider(scrapy.Spider):
     allowed_domains = ["careers.stackoverflow.com"]
 
     def start_requests(self):
-        for i in xrange(1, 100):
+        for i in xrange(1, 2):
             yield self.make_requests_from_url(
                 "http://careers.stackoverflow.com/jobs/?pg=%d" % i)
 
     def parse(self, response):
-        # <div data-jobid="68943">...</div>
-        # <h3><a class="job-link"
-        # href="/jobs/69275/systems-engineer-amazon?a=5CQU76fq6s
-        # title="Systems Engineer">Systems Engineer</a></h3>
-        # <p class="tags">
-        # <a class="post-tag job-link" href="/jobs/tag/python>python</a></p>
+        # Get job id, title, tags, date, location, employer, url
+        # Make request to job detail url
+
         jobs = response.xpath('//div[@data-jobid]')
-        if not jobs:
-            raise CloseSpider('No more job pages')
 
         for sel in jobs:
             job = JobItem()
-            job['id'] = sel.xpath('.//@data-jobid')[0].extract()
+            jobid = sel.xpath('.//@data-jobid')[0].extract()
+            job['id'] = jobid
             job['title'] = sel.xpath('.//h3/a/text()')[0].extract()
             job['tags'] = sel.css('.post-tag::text').extract()
-            yield job
+            request = scrapy.Request("http://careers.stackoverflow.com/jobs/" +
+                                     jobid,
+                                     callback=self.parse_job_detail_page)
+            request.meta['job'] = job
+            yield request
+
+    def parse_job_detail_page(self, response):
+        # Get job description
+        job = response.meta['job']
+        job['url'] = response.url
+        return job
