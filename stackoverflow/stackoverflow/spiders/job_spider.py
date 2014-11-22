123f3d30e48b044fe33eea8badee632f9b85c587
arimbr <arimbr@gmail.com>
2014-11-22 15:33:59 +0200

    Set job employer and location

--- stackoverflow/stackoverflow/spiders/job_spider.py
+++ stackoverflow/stackoverflow/spiders/job_spider.py
@@ -16,18 +16,23 @@ class JobSpider(scrapy.Spider):
                 "http://careers.stackoverflow.com/jobs/?pg=%d" % i)
 
     def parse(self, response):
-        # Get job id, title, tags, date, location, employer, url
-        # Make request to job detail url
-
+        """
+        Get job id, date, title, employer, location, tags
+        Returns request object with job data
+        """
         jobs = response.xpath('//div[@data-jobid]')
 
         for sel in jobs:
             job = JobItem()
             jobid = sel.xpath('.//@data-jobid')[0].extract()
+            location = sel.css('.location::text')[1].extract() \
+                          .strip().split()[1:]
             job['id'] = jobid
+            job['date'] = self.start_time.isoformat()
             job['title'] = sel.xpath('.//h3/a/text()')[0].extract()
+            job['employer'] = sel.css('.-employer::text')[0].extract()
+            job['location'] = ' '.join(location).split(', ')
             job['tags'] = sel.css('.post-tag::text').extract()
-            job['date'] = str(self.start_time)
             request = scrapy.Request("http://careers.stackoverflow.com/jobs/" +
                                      jobid,
                                      callback=self.parse_job_detail_page)
@@ -35,7 +40,10 @@ class JobSpider(scrapy.Spider):
             yield request
 
     def parse_job_detail_page(self, response):
-        # Get job description
+        """
+        Get job url, description
+        Returns job item to pipeline
+        """
         job = response.meta['job']
         job['url'] = response.url
         return job
